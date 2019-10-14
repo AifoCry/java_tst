b6a3997b42a8181561f2b73e123d4ce0242133d6
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-10-14 13:40:37 +0300

    +обьект и конструктор

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,12 +3,8 @@ package ru.stqa.pft.sandbox;
  public class MyFirstProgram {
 
     public static void main(String[] args) {
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x = 10.0;
-        p1.y = 20.0;
-        p2.x = 30.0;
-        p2.y = 40.0;
+        Point p1 = new Point(10.0,20.0);
+        Point p2 = new Point(30.0,40.0);
         System.out.println("Растояние между точек P1 и P2 = " + distance(p1, p2));
  }
 
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,6 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
-    Double x,y;
+    public Double x;
+    public Double y;
 
+    public Point(double x, double y) {
+        this.y = y;
+        this.x = x;
+    }
 }
