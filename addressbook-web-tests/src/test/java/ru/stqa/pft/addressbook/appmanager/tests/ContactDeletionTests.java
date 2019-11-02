e09cc1766e2637468757dcf7acab519c7aca77fc
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-02 17:41:37 +0300

    Коллекции. Кол-во элементов в коллекции. Группы.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.appmanager.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.model.ContactData;
 
@@ -12,9 +13,12 @@ public class ContactDeletionTests extends TestBase {
         if (! app.getContactHelper().isThereAContact()){
             app.getContactHelper().createContact(new ContactData("Alexandr", "Eliseev", "+79167777777", "alex@yandex.ru", "test1"),true);
         }
+        int before = app.getGroupHelper().getGroupCount();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectContact();
         app.getContactHelper().isAlertPresent();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after,before -1);
     }
 
 }
