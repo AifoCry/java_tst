95939097cbde0852450058b40f95071c26432263
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-13 16:56:17 +0300

    5.5 лекция для тестов групп. переход на множества

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupDeletionTests.java
@@ -6,29 +6,31 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 
 import java.util.List;
+import java.util.Set;
 
 public class GroupDeletionTests extends TestBase {
 
   @BeforeMethod
   public void ensurePreconditions() {
     app.goTo().groupPage();
-    if (app.group().list().size() == 0){
+    if (app.group().all().size() == 0){
       app.group().create(new GroupData().WithName("test1"));
     }
   }
 
   @Test
   public void testGroupDeletion() throws Exception {
-    List<GroupData> before = app.group().list();
-    int index = before.size()-1;
+    Set<GroupData> before = app.group().all();
+    GroupData deletedGroup = before.iterator().next();
+    //int index = before.size()-1;
     //int before = app.getGroupHelper().getGroupCount();
-    app.group().delete(index);
-    List<GroupData> after = app.group().list();
+    app.group().delete(deletedGroup);
+    Set<GroupData> after = app.group().all();
    // int after = app.getGroupHelper().getGroupCount();
     //Assert.assertEquals(after,before - 1 );
     Assert.assertEquals(after.size(),before.size() - 1 );
 
-    before.remove(index);
+    before.remove(deletedGroup);
     Assert.assertEquals(before,after);
 
   }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupModificationTests.java
@@ -7,35 +7,39 @@ import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions() {
         app.goTo().groupPage();
-        if (app.group().list().size() == 0) {
+        if (app.group().all().size() == 0) {
             app.group().create(new GroupData().WithName("test1"));
         }
     }
 
     @Test
     public void testGroupModification() throws Exception {
-        List<GroupData> before = app.group().list();
-        int index = before.size() - 1;
+        Set<GroupData> before = app.group().all();
+        GroupData modifiedGroup = before.iterator().next();
+        // int index = before.size() - 1;
         GroupData group = new GroupData()
-                .WithId(before.get(index).getId()).WithName("test1").WithFooter("test2").WithHeader("test3");
+                .WithId(modifiedGroup.getId()).WithName("test1").WithFooter("test2").WithHeader("test3");
         //int before = app.getGroupHelper().getGroupCount();
-        app.group().modify(index, group);
-        List<GroupData> after = app.group().list();
+        app.group().modify(group);
+        Set<GroupData> after = app.group().all();
        // int after = app.getGroupHelper().getGroupCount();
        // Assert.assertEquals(after,before );
         Assert.assertEquals(after.size(),before.size() );
 
-        before.remove(index);
+        before.remove(modifiedGroup);
         before.add(group);
+        /* Сортировка
         Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
         before.sort(byId);
         after.sort(byId);
+        */
         Assert.assertEquals(before,after);
 
 
