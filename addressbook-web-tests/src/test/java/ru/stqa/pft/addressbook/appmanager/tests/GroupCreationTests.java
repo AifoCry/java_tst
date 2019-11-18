07153499f5e20f4b204dc49eae7a617b4e8bc798
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-19 01:07:08 +0300

    6.7 + json генератор и чтение из файла

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.addressbook.appmanager.tests;
 
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 import ru.stqa.pft.addressbook.appmanager.model.Groups;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -18,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
         //List<Object[]> list = new ArrayList<Object[]>();
         BufferedReader reader =  new BufferedReader(new FileReader( new File("src/test/resources/groups.xml")));
         String xml = "";
@@ -39,7 +40,21 @@ public class GroupCreationTests extends TestBase {
         //return list.iterator();
     }
 
-    @Test(dataProvider = "validGroups")
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader reader =  new BufferedReader(new FileReader( new File("src/test/resources/groups.json")));
+        String json = "";
+        String line = reader.readLine();
+        while (line !=null) {
+            json += line;
+            line = reader.readLine();
+        }
+        Gson gson = new Gson();
+        List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>(){}.getType());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validGroupsFromJson")
     public void testGroupCreation(GroupData group) throws Exception {
         //GroupData group = new GroupData().WithName(name).WithHeader(header).WithFooter(footer);
         app.goTo().groupPage();
