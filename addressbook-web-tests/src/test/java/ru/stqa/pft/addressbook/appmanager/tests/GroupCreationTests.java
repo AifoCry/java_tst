5ad94f007729c0d1258740e7bfab22f3304c2eb9
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-19 00:43:34 +0300

    6.6 + чтение из XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.appmanager.tests;
 
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 import ru.stqa.pft.addressbook.appmanager.model.Groups;
@@ -9,6 +10,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -17,18 +19,24 @@ public class GroupCreationTests extends TestBase {
 
     @DataProvider
     public Iterator<Object[]> validGroups() throws IOException {
-        List<Object[]> list = new ArrayList<Object[]>();
-        BufferedReader reader =  new BufferedReader(new FileReader( new File("src/test/resources/groups.csv")));
+        //List<Object[]> list = new ArrayList<Object[]>();
+        BufferedReader reader =  new BufferedReader(new FileReader( new File("src/test/resources/groups.xml")));
+        String xml = "";
         String line = reader.readLine();
         while (line !=null) {
-            String[] split = line.split(";");
-            list.add (new Object[] {new GroupData().WithName(split[0]).WithHeader(split[1]).WithFooter(split[2])});
+            xml += line;
+            //String[] split = line.split(";");  для csv
+            //list.add (new Object[] {new GroupData().WithName(split[0]).WithHeader(split[1]).WithFooter(split[2])});
             line = reader.readLine();
         }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
         //list.add(new Object[]{new GroupData().WithName("test1").WithHeader("header 1").WithFooter("footer 1")});
         //list.add(new Object[]{new GroupData().WithName("test2").WithHeader("header 2").WithFooter("footer 2")});
         //list.add(new Object[]{new GroupData().WithName("test3").WithHeader("header 3").WithFooter("footer 3")});
-        return list.iterator();
+        //return list.iterator();
     }
 
     @Test(dataProvider = "validGroups")
