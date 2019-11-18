b84b539b35bfeb36e30a8a155097918ef009bea4
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-18 23:27:48 +0300

    6.5 +чтение из файла groups.csv

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/tests/GroupCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.model.GroupData;
 import ru.stqa.pft.addressbook.appmanager.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,11 +16,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
+    public Iterator<Object[]> validGroups() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[]{new GroupData().WithName("test1").WithHeader("header 1").WithFooter("footer 1")});
-        list.add(new Object[]{new GroupData().WithName("test2").WithHeader("header 2").WithFooter("footer 2")});
-        list.add(new Object[]{new GroupData().WithName("test3").WithHeader("header 3").WithFooter("footer 3")});
+        BufferedReader reader =  new BufferedReader(new FileReader( new File("src/test/resources/groups.csv")));
+        String line = reader.readLine();
+        while (line !=null) {
+            String[] split = line.split(";");
+            list.add (new Object[] {new GroupData().WithName(split[0]).WithHeader(split[1]).WithFooter(split[2])});
+            line = reader.readLine();
+        }
+        //list.add(new Object[]{new GroupData().WithName("test1").WithHeader("header 1").WithFooter("footer 1")});
+        //list.add(new Object[]{new GroupData().WithName("test2").WithHeader("header 2").WithFooter("footer 2")});
+        //list.add(new Object[]{new GroupData().WithName("test3").WithHeader("header 3").WithFooter("footer 3")});
         return list.iterator();
     }
 
