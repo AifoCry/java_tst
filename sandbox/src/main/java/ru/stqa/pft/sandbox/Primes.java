14eba1231729d00ea6f221d8e05604d379a95c65
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-11-20 19:05:36 +0300

    6.9 конфиг файлы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Primes.java
@@ -19,7 +19,7 @@ public class Primes {
     }
 
     public static boolean isPrime(long n) {
-        for (long i = 2; i < n; i = i++) {
+        for (long i = 2; i < n; i++) {
             if (n % i == 0) {
                 return false;
             }
@@ -28,7 +28,7 @@ public class Primes {
     }
     public static boolean isPrimeFast(int n) {
         int m = (int)Math.sqrt(n);
-        for (int i = 2; i < m / 2; i = i++) {
+        for (int i = 2; i < m / 2;  i++) {
             if (n % i == 0) {
                 return false;
             }
