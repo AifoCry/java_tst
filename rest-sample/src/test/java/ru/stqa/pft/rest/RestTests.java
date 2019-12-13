44ed22df1a768cf7d7cac855958f7724de909fb5
AifoCry <56160310+AifoCry@users.noreply.github.com>
2019-12-13 18:24:58 +0300

    Задание 20

--- rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
+++ rest-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -15,12 +15,12 @@ import java.util.Set;
 
 import static org.testng.Assert.assertEquals;
 
-public class RestTests {
+public class RestTests extends TestBase {
 
     @Test
     public void testCreateIssue() throws IOException {
         Set<Issue> oldIssues = getIssues();
-        Issue newIssue = new Issue().withSubject("Test issue").withDescription("Test description");
+        Issue newIssue = new Issue().withSubject("Test123").withDescription("MYYY");
         int issueId = createIssue(newIssue);
         Set<Issue> newIssues = getIssues();
         oldIssues.add(newIssue.withId(issueId));
@@ -29,7 +29,7 @@ public class RestTests {
 
 
     private Set<Issue> getIssues() throws IOException {
-        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json/"))
+        String json = getExecutor().execute(Request.Get("https://bugify.stqa.ru/api/issues.json?limit=100"))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         JsonElement issues = parsed.getAsJsonObject().get("issues");
@@ -38,17 +38,14 @@ public class RestTests {
     }
 
     private int createIssue(Issue newIssue) throws IOException {
-        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json/")
+        int issueId = 2164;
+        skipIfNotFixed(issueId);
+        System.out.println("Create new issue because ID " + issueId + " is fixed");
+        String json = getExecutor().execute(Request.Post("https://bugify.stqa.ru/api/issues.json?limit=100")
                 .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject())
                         , new BasicNameValuePair("description", newIssue.getDescription())))
                 .returnContent().asString();
         JsonElement parsed = new JsonParser().parse(json);
         return parsed.getAsJsonObject().get("issue_id").getAsInt();
     }
-
-    public Executor getExecutor() {
-        return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
-    }
-
-
 }
